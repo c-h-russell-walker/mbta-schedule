e2da544cc662a9adad8b6513064636e14a19f96a
Chris Russell-Walker <c.russell.walker@gmail.com>
2019-03-03 11:45:00 -0500

    CRW Hooked up stubbed message/event handlers

--- src/components/DeparturesTable.jsx.js
+++ src/components/DeparturesTable.jsx.js
@@ -12,6 +12,73 @@ class DeparturesTable extends React.Component {
     this.state = {
       predictions: [],
     };
+
+    this.eventSourceOnMessage = this.eventSourceOnMessage.bind(this);
+    this.eventSourceOnError = this.eventSourceOnError.bind(this);
+    this.eventSourceOnOpen = this.eventSourceOnOpen.bind(this);
+
+    this.resetEvent = this.resetEvent.bind(this);
+    this.addEvent = this.addEvent.bind(this);
+    this.updateEvent = this.updateEvent.bind(this);
+    this.removeEvent = this.removeEvent.bind(this);
+
+    this._setupEventSource();
+  }
+
+  _setupEventSource() {
+    const baseUrl = 'https://api-v3.mbta.com/';
+    const apiKey = process.env.REACT_APP_MBTA_KEY;
+
+    // TODO - Use url parsing in JS
+    // TODO - Set these up to be props and/or configurable
+
+    const mbtaEventSourceUrl = `${baseUrl}predictions/?api_key=${apiKey}&filter[stop]=place-north,place-sstat&filter[route_type]=2&filter[direction_id]=0&sort=departure_time&include=schedule,trip.direction_id,route.type`;
+
+    const evtSource = new EventSource(mbtaEventSourceUrl);
+
+    evtSource.onmessage = this.eventSourceOnMessage;
+    evtSource.onerror = this.eventSourceOnError;
+    evtSource.onopen = this.eventSourceOnOpen;
+
+    evtSource.addEventListener('reset', this.resetEvent, false);
+    evtSource.addEventListener('add', this.addEvent, false);
+    evtSource.addEventListener('update', this.updateEvent, false);
+    evtSource.addEventListener('remove', this.removeEvent, false);
+  }
+
+  eventSourceOnMessage(evt) {
+    console.log('onmessage');
+    console.log(evt);
+  }
+
+  eventSourceOnError(evt) {
+    console.log('onerror');
+    console.log(evt);
+  }
+
+  eventSourceOnOpen(evt) {
+    console.log('onopen');
+    console.log(evt);
+  }
+
+  resetEvent(evt) {
+    console.log('resetEvent !!');
+    console.log(evt);
+  }
+
+  addEvent(evt) {
+    console.log('addEvent !!');
+    console.log(evt);
+  }
+
+  updateEvent(evt) {
+    console.log('updateEvent !!');
+    console.log(evt);
+  }
+
+  removeEvent(evt) {
+    console.log('removeEvent !!');
+    console.log(evt);
   }
 
   render() {
